8029a6d389f9f6192136d926e5740a48ee93cadc
Roland Swingler <roland.swingler@gmail.com>
2010-02-22 13:11:50 +0000

    Adding ruby mode hooks for non .rb files

--- custom/roland-hooks.el
+++ custom/roland-hooks.el
@@ -1,12 +1,10 @@
 ;; Espresso mode hooks
 
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-
 (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 (add-hook 'espresso-mode-hook 'moz-minor-mode)
+(setq espresso-indent-level 2)
 
 (eval-after-load 'espresso
   '(progn ;; fixes problem with pretty function font-lock
@@ -19,5 +17,19 @@
                              (0 (progn (compose-region (match-beginning 1)
                                                        (match-end 1) "ƒ")
                                        nil)))))))
+
+
+;; Ruby/Feature mode hooks
+
+(require 'feature-mode)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;; Rake files are ruby, too, as are gemspecs, rackup files, and gemfiles.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
 
 (provide 'roland-hooks)

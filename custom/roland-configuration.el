905b6da5e684c53022c28db59be00446848543b8
Roland Swingler <roland.swingler@gmail.com>
2010-11-14 21:34:30 +0000

    ido mode config

--- custom/roland-configuration.el
+++ custom/roland-configuration.el
@@ -20,11 +20,12 @@
 ;; IDO
 (ido-mode t)
 (setq ido-enable-prefix nil
-      ido-use-filename-at-point -1
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-max-prospects 10)
+      ido-use-filename-at-point nil
+      ido-max-prospects 10
+      ido-auto-merge-work-directories-length -1
+      ido-case-fold t)
 
 ;; CSS Mode
 (setq cssm-indent-level 2)
